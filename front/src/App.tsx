4c633bef9a2f1dbdac29ae3c07c23ea5765f7716
JeanFSZ <jeanferfs30@gmail.com>
2024-10-22 22:34:43 +0200

    Add Button Shadcn Component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
+      <Button>
+        Hola
+      </Button>
     </h1>
+    
   )
 }
